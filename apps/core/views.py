1bce6a951ac871262e1628d0cdf17c5c600c24b7
Bleu Serge Alain <realsergealain@gmail.com>
2026-08-30 03:46:21 +0200

    🐛 Fix des imports et application de la migration de stock

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,6 @@
+import hashlib
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
